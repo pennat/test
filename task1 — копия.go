02113039d0410bb49c0b28dfc432d17810b599cc
pennat <penkova.nataly@yandex.ru>
2022-04-11 12:02:17 +0300

    task

---
+++ task1 — копия.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b int
+
+	fmt.Println("Введите сторону a")
+	fmt.Scanln(&a)
+
+	fmt.Println("Введите сторону b")
+	fmt.Scanln(&b)
+
+	fmt.Println("Площадь прямоугольника:", a*b)
+}
